4a175df1c61adfa6a7d0a755c430bc5b095bf595
saadvsy <122667630+saadvsy@users.noreply.github.com>
2024-07-11 07:12:41 +0000

    functions, scopes, arrow function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,11 +28,41 @@ const result = addTwoNumbers(3, 5)
 
 function loginUserMessage(username = "sam"){
     if(!username){
-        console.log("Please enter a username")
+        // console.log("Please enter a username")
         return
     }
     return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("Saad"))
-console.log(loginUserMessage("saad"))
+// console.log(loginUserMessage("saad"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "saad",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
