2810975975ad30687794a8d7fff505e3c1220360
saadvsy <122667630+saadvsy@users.noreply.github.com>
2024-07-13 16:50:15 +0000

    iterations continued

---
+++ 05_iterations/seven.js
@@ -0,0 +1,20 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => num + 10)
+
+// console.log(newNums);
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+// console.log(newNums);
+
+// myNumers.forEach( (num) => 
+//     console.log(num)
+// )
+
+// Chaining
+const newNums = myNumers
+                .map( (num) => num * 10 )
+                .map( (num) => num + 1 )
+                .filter( (num) => num >= 40 )
+
+console.log(newNums);
